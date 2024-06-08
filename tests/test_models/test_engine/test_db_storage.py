6568f742478ad7979615f0b940b2ab68f3c87ab0
Muhairwe144J <mnasasirashafik144j@gmail.com>
2024-06-08 09:46:07 +0300

    AirBnB_clone_v3

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -2,13 +2,22 @@
 """
 Unit Test for BaseModel Class
 """
-import unittest
 from datetime import datetime
-from models import *
+import inspect
+import models
+from models.engine import db_storage
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+import json
 import os
-from models.base_model import Base
-from models.engine.db_storage import DBStorage
-
+import pep8
+import unittest
+from models import *
 
 storage_type = os.environ.get('HBNB_TYPE_STORAGE')
 
